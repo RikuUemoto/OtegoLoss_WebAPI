655b91abd2f021b88a9d5ab865f3745f0fc2983b
RrkkU <72247484+RrkkU@users.noreply.github.com>
2022-01-12 01:50:32 +0900

    （アカウント削除前）購入IDと配送状況を返すWebAPIを作成．

---
+++ Purchase/PurchaseDelistatus.php
@@ -0,0 +1,48 @@
+<?php
+/*
+    作成者：植元 陸
+    最終更新日：2022/1/12
+    目的：  （アカウント削除前）購入IDと配送状況を返す
+    入力：  user_id
+    http通信例：
+    http://localhost/OtegoLoss_WebAPI/purchase/purchasedelistatus.php?user_id=u0000005
+    
+    その他：
+*/
+
+//json形式ファイルのheader
+header("Content-Type: application/json; charset=utf-8");
+
+// DBとの連携
+try{
+    $db = new PDO('mysql:dbname=test;host=localhost;charset=utf8','root','root');
+    echo "接続OK";
+    // データベース
+    $data = "purchase";
+
+    if(isset($_GET["user_id"])) {
+        // numをエスケープ(xss対策)
+        $param = htmlspecialchars($_GET["user_id"]);
+        //SQL構文
+        $table2 = "SELECT purchase_id, delivery_status
+                    FROM $data
+                    WHERE purchaser_id = '$param'";
+
+        // メイン処理
+        $arr["status"] = "yes";
+        $sql2 = $db->query($table2);
+        
+        $arr = $sql2 -> fetchAll();
+
+    } else {
+        // paramの値が不適ならstatusをnoにしてプログラム終了
+        $arr["status"] = "no";
+    }
+
+    // 配列をjson形式にデコードして出力, 第二引数は、整形するためのオプション
+    print json_encode($arr, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
+} catch(PDOException $e) {
+    echo "error".$e->getMessage();
+}
+?>
