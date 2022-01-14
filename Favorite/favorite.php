cf5574e33655b8d211f03724e69aed713e81d6ae
gosuperu7 <gosuperu7@icloud.com>
2022-01-14 11:36:46 +0900

    お気に入りテーブルにお気に入り情報を追加するWebAPIを作成 お気に入り情報を返すWebAPIを作成

---
+++ Favorite/favorite.php
@@ -0,0 +1,44 @@
+<?php
+/*
+    作成者:坂口　白磨
+    最終更新日:2022/1/
+    目的:  お気に入り情報を返す
+    http通信例:
+    http://localhost/software_engineering/Favorite/favorite.php?favorite_id=
+    
+    その他:
+*/
+
+//json形式ファイルのheader
+header("Content-Type: application/json; charset=utf-8");
+
+// DBとの連携
+try{
+    $db = new PDO('mysql:dbname=software;host=localhost;charset=utf8','root','root');
+    echo "接続OK";
+
+    if(isset($_GET["favorite_id"])) {
+        // numをエスケープ(xss対策)
+        $param = htmlspecialchars($_GET["favorite_id"]);
+        //SQL構文
+        $table2 = "SELECT favorite_id, user_id, favorite_user_id
+                     FROM favorite
+                     WHERE favorite_id = '$param'";
+        // メイン処理
+        $arr["status"] = "yes";
+        $sql2 = $db->query($table2);
+        
+        $arr = $sql2 -> fetchAll(PDO::FETCH_ASSOC);
+
+    } else {
+        // paramの値が不適ならstatusをnoにしてプログラム終了
+        $arr["status"] = "no";
+    }
+
+    // 配列をjson形式にデコードして出力, 第二引数は、整形するためのオプション
+    print json_encode($arr, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
+} catch(PDOException $e) {
+    echo "error".$e->getMessage();
+}
+?>
