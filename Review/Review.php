e217fdf17d3349d14ab496fcf6fbc2eb5d52224f
gosuperu7 <gosuperu7@icloud.com>
2022-01-20 15:38:02 +0900

    入力をuser_idに訂正

--- Review/Review.php
+++ Review/Review.php
@@ -3,9 +3,9 @@
     作成者：松尾 匠馬
     最終更新日：2022/1/19
     目的：  レビューテーブルの生産者ID(ユーザID)からレビュー情報を返す(出品者情報)
-    入力：  review_id
+    入力：  user_id
     http通信例：
-    http://localhost/OtegoLoss_WebAPI/product/Review.php?review_id=u0000111
+    http://localhost/OtegoLoss_WebAPI/product/Review.php?user_id=u0000111
     
     その他：
 */
@@ -20,13 +20,13 @@ try{
     // データベース
     $data_review = "review";
 
-    if(isset($_GET["review_id"])) {
+    if(isset($_GET["user_id"])) {
         // numをエスケープ(xss対策)
-        $param = htmlspecialchars($_GET["review_id"]);
+        $param = htmlspecialchars($_GET["user_id"]);
         //SQL構文
         $table2 = "SELECT user_id, review_user_id, assessment, comment
                      FROM $data_review
-                     WHERE review_id = '$param'";
+                     WHERE user_id = '$param'";
         // メイン処理
         $arr["status"] = "yes";
         $sql2 = $db->query($table2);
